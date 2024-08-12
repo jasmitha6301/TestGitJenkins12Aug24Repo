447f1ed2b79864c9e4bfe07640e8857d9c58badf
saiteja <mamidisrisaiteja@gmail.com>
2024-08-12 15:47:28 +0530

    3 rd commit

--- src/test/java/stepDefinition/StepDefinitionDemo.java
+++ src/test/java/stepDefinition/StepDefinitionDemo.java
@@ -7,6 +7,7 @@ public class StepDefinitionDemo {
 	@Given("I want to write a step with precondition")
 	public void i_want_to_write_a_step_with_precondition() {
 	    System.out.println("In the first Scenario");
+	    System.out.println("In the first Scenario 2nd time");
 	}
 	
 
